a6187ee38dea2187ae5e61592b16d33bf898df01
nitinguptamca <nitinguptamca@gmail.com>
2023-09-30 18:09:55 +0530

    Date 30-Sep-2023-11

---
+++ infrastructure/saga/src/main/java/book/my/show/saga/SagaStep.java
@@ -0,0 +1,8 @@
+package book.my.show.saga;
+
+import book.my.show.booking.service.domain.event.DomainEvent;
+
+public interface SagaStep<T, S extends DomainEvent, U extends DomainEvent> {
+    S process(T data);
+    U rollback(T data);
+}
