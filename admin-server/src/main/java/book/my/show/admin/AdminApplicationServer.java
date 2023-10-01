1a44548b419eddfb3161da0e74163fdc659878dd
nitinguptamca <nitinguptamca@gmail.com>
2023-10-01 13:31:55 +0530

    Date 30-Sep-2023-23

---
+++ admin-server/src/main/java/book/my/show/admin/AdminApplicationServer.java
@@ -0,0 +1,15 @@
+package book.my.show.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class AdminApplicationServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplicationServer.class ,args);
+    }
+}
---
+++ discovery-server/src/main/java/book/my/show/discovery/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package book.my.show.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.swing.Spring;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class,args);
+    }
+}
