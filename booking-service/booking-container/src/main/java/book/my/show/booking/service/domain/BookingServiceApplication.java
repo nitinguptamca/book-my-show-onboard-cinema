a3b21184e6696a8c6faed2fabc9460163179813c
nitinguptamca <nitinguptamca@gmail.com>
2023-10-01 19:31:26 +0530

    Date 30-Sep-2023-25

--- booking-service/booking-container/src/main/java/book/my/show/booking/service/domain/BookingServiceApplication.java
+++ booking-service/booking-container/src/main/java/book/my/show/booking/service/domain/BookingServiceApplication.java
@@ -3,13 +3,15 @@ package book.my.show.booking.service.domain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = { "book.my.show.booking.service.dataaccess", "book.my.show.dataaccess" })
 @EntityScan(basePackages = { "book.my.show.booking.service.dataaccess", "book.my.show.dataaccess"})
 @SpringBootApplication(scanBasePackages = "book.my.show")
-public class OrderServiceApplication {
+@EnableDiscoveryClient
+public class BookingServiceApplication {
     public static void main(String[] args) {
-      SpringApplication.run(OrderServiceApplication.class, args);
+      SpringApplication.run(BookingServiceApplication.class, args);
     }
 }
--- payment-service/payment-container/src/main/java/book/my/show/payment/service/domain/PaymentServiceApplication.java
+++ payment-service/payment-container/src/main/java/book/my/show/payment/service/domain/PaymentServiceApplication.java
@@ -3,11 +3,13 @@ package book.my.show.payment.service.domain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "book.my.show.payment.service.dataaccess")
 @EntityScan(basePackages = "book.my.show.payment.service.dataaccess")
 @SpringBootApplication(scanBasePackages = "book.my.show")
+@EnableDiscoveryClient
 public class PaymentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
